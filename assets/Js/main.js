4bfd19c9ba424e228ef6b63fdfafad294421116e
Turkan Qeniyeva <turkanqnyeva0@gmail.com>
2024-12-12 00:22:52 +0400

    first commit

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -3,3 +3,47 @@ let menu = document.querySelector(".menuu")
 menubutton.addEventListener('click', () =>{
 menu.classList.toggle("miniMenu")
 });
+
+
+
+let products = document.getElementById("products")
+
+const getProducts = async () => {
+    try {
+        const response = await axios.get("https://fakestoreapi.com/products");
+        const products = response.data;
+
+
+        const productList = document.getElementById('product-list');
+        productList.innerHTML = '';
+
+        products.forEach(product => {
+            const productCard = `
+                <div class="col-md-2 d-flex flex-column align-items-center boxx">
+                    <div class="imgg">
+                        <img src="${product.image}" alt="${product.title}">
+                    </div>
+                    <h3>${product.title}</h3>
+                    <div class="text">
+                        <span class="sale price">$${product.price}</span>
+                        <div class="icons">
+                            <i class="fa-solid fa-bars"></i>
+                            <i class="fa-solid fa-cart-shopping"></i>
+                            <i class="fa-solid fa-heart"></i>
+                        </div>
+                    </div>
+                </div>
+            `;
+
+            productList.innerHTML += productCard;
+        });
+    } catch (error) {
+        console.error('Xeta bas verdi', error);
+    }
+};
+
+
+window.onload = getProducts;
+
+
+
